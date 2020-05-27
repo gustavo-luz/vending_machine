4de5b699c1e87c7b9fdc99d152fbebbb69ed7e6f
gustavo luz <58995773+gustavo-luz@users.noreply.github.com>
2020-05-27 19:19:20 -0300

    rele com vending v1

---
+++ motor_rele1.ino
@@ -0,0 +1,28 @@
+#define CW 7 //CW is defined as pin #7//
+
+#define CCW 8 //CCW is defined as pin #8//
+
+void setup() { //Setup runs once//
+
+pinMode(CW, OUTPUT); //Set CW as an output//
+
+pinMode(CCW, OUTPUT); //Set CCW as an output//
+
+}
+
+void loop() { //Loop runs forever//
+
+digitalWrite(CW,HIGH); //Motor runs clockwise//
+
+delay(5000); //for 1 second//
+
+digitalWrite(CW, LOW); //Motor stops//
+delay(10000);
+
+digitalWrite(CCW, HIGH);//Motor runs counter-clockwise//
+
+delay(5000); //For 1 second//
+
+digitalWrite(CCW, LOW); //Motor stops//
+
+}
